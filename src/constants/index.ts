3bf18315c59c0a5b65e4bfd1cc3bf92a6583eff0
extazystas <2153311+Extazystas@users.noreply.github.com>
2024-03-31 20:31:02 +0300

    Add constants index

---
+++ src/constants/index.ts
@@ -0,0 +1,9 @@
+export const DEFAULT_WIDGET_PARAMS = {
+  latitude: 60.16952,
+  longitude: 24.93545,
+  days: 1,
+  theme: 'dark'
+};
+
+export const GEODOING_API_URL = import.meta.env.APP_GEODOING_API_URL || 'https://geocoding-api.open-meteo.com/v1'
+export const GRAPHQL_API_URL  = import.meta.env.APP_GRAPHQL_API_URL || 'http://localhost:4445/graphql'
